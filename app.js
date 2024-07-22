1e07a990bd5af01415c0d8cf583a96b4610a7a7f
hello-world-ttj <tijotjoseph003@gmail.com>
2024-07-22 13:55:38 +0530

    #secrets

--- app.js
+++ app.js
@@ -13,55 +13,72 @@ const adminRoute = require("./src/routes/admin");
 const counsellorRoute = require("./src/routes/counsellor");
 const userRoute = require("./src/routes/user");
 const initializeTables = require("./src/helpers/tableInitialization");
+const loadSecrets = require("./src/config/env.config");
 const app = express();
 app.use(volleyball);
 
-//* Define the PORT & API version based on environment variable
-const { PORT, API_VERSION, NODE_ENV } = process.env;
-//* Enable Cross-Origin Resource Sharing (CORS) middleware
-app.use(cors());
-//* Parse JSON request bodies
-app.use(express.json());
-//* Set the base path for API routes
-const BASE_PATH = `/api/${API_VERSION}`;
-//* Import database connection module
-require("./src/helpers/connection");
-// //! Uncomment this if you don't have a tables created
-// initializeTables()
-//   .then(() => {
-//     console.log(
-//       clc.magentaBright("Table initialization completed successfully.")
-//     );
-//   })
-//   .catch((error) => {
-//     console.error("Error initializing tables:", error);
-//   });
+const NODE_ENV = process.env.NODE_ENV;
 
-//* Swagger setup
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, swaggerOptions)
-);
+//* Function to start the server
+const startServer = async () => {
+  try {
+    if (NODE_ENV === "production") {
+      await loadSecrets();
+    }
+    //* Define the PORT & API version based on environment variable
+    const { PORT, API_VERSION } = process.env;
+    //* Enable Cross-Origin Resource Sharing (CORS) middleware
+    app.use(cors());
+    //* Parse JSON request bodies
+    app.use(express.json());
+    //* Set the base path for API routes
+    const BASE_PATH = `/api/${API_VERSION}`;
+    //* Import database connection module
+    require("./src/helpers/connection");
+    // //! Uncomment this if you don't have a tables created
+    // initializeTables()
+    //   .then(() => {
+    //     console.log(
+    //       clc.magentaBright("Table initialization completed successfully.")
+    //     );
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error initializing tables:", error);
+    //   });
 
-//* Configure routes for user API
-app.use(`${BASE_PATH}/admin`, adminRoute);
-app.use(`${BASE_PATH}/counsellor`, counsellorRoute);
-app.use(`${BASE_PATH}/user`, userRoute);
+    //* Swagger setup
+    app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerSpec, swaggerOptions)
+    );
 
-//? Define a route for the API root
-app.get(BASE_PATH, (req, res) => {
-  return responseHandler(
-    res,
-    200,
-    "🛡️ Welcome! All endpoints are fortified. Do you possess the master 🗝️?",
-    null
-  );
-});
+    //* Configure routes for user API
+    app.use(`${BASE_PATH}/admin`, adminRoute);
+    app.use(`${BASE_PATH}/counsellor`, counsellorRoute);
+    app.use(`${BASE_PATH}/user`, userRoute);
 
-//! Start the server and listen on the specified port from environment variable
-app.listen(PORT, () => {
-  const portMessage = clc.redBright(`✓ App is running on port: ${PORT}`);
-  const envMessage = clc.yellowBright(`✓ Environment: ${NODE_ENV}`);
-  console.log(`${portMessage}\n${envMessage}`);
-});
+    //? Define a route for the API root
+    app.get(BASE_PATH, (req, res) => {
+      return responseHandler(
+        res,
+        200,
+        "🛡️ Welcome! All endpoints are fortified. Do you possess the master 🗝️?",
+        null
+      );
+    });
+
+    //! Start the server and listen on the specified port from environment variable
+    app.listen(PORT, () => {
+      const portMessage = clc.redBright(`✓ App is running on port: ${PORT}`);
+      const envMessage = clc.yellowBright(`✓ Environment: ${NODE_ENV}`);
+      console.log(`${portMessage}\n${envMessage}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+    process.exit(1); // Exit the application with a non-zero status code
+  }
+};
+
+//! Start the servers
+startServer();
